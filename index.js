fd8e66df37b99daabcb6ce5e9212e4196422cc78
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-24 02:37:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ function nestedTarget(){
 function deepestChild(){
   const child = document.getElementById('app').querySelectorAll('div#grande-node div div div div');
   var test = child[i].innerHTML;
-  for (let i = 0; i < child.length; i++) {
+  var i = 0;
+  while (i<child.length) {
     child[i].innerHTML = (i + 1).toString();
+    i++;
   }
-  return child;
+  return test;
 }
 function increaseRankBy(n){
   let child = document.getElementById('app').querySelectorAll('ul.ranked-list li')
