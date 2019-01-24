0b3e3e2fde9cdd62b8469527dd6bda629c92b120
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-24 02:31:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(''+selector);
+}
+function nestedTarget(){
+  return document.querySelector('div#nested div div div div.target');
+}
+function deepestChild(){
+  const child = document.getElementById('app').querySelectorAll('div#grande-node');
 
+  return ;
+}
+function increaseRankBy(n){
+  let child = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(let i = 0; i<child.length; i++){
+    child[i].innerHTML = parseInt(child[i].innerHTML)+n;
+  }
+}
